1f86b802e1f586c0ae81e7e0844ef676d9ea3fb1
Athirach <wathirach@gmail.com>
2024-08-14 21:55:43 +0700

    BP5-Wan Assignment Value Variable

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let firstName = "Karter";
+let lastName = "Anderson";
+let Address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const retuarantName = "Chef's Table by Chef James";
+const totalAmount = 15000;
+const totalOpen = 1500;
 //Exercise #2: Variable Reassignment
 //Start coding here
+Address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalAmount = 5000; //error const cannot reassign to variable
+totalOpen = 2500; //error const cannot reassign to variable
+
